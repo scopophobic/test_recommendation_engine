1a8ff46f4079e57ef8c66f829a8cbad635fcb6d8
scopophobic <adithyanmadhu1234@gmail.com>
2025-04-07 09:24:17 +0530

    fastAPI Build is done

---
+++ app/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from typing import List, Any
+from app.utils.recommender import recommend_tests
+
+app = FastAPI()
+
+
+class PromptInput(BaseModel):
+    prompt: str
+
+class PromptRequest(BaseModel):
+    prompt: str
+    top_k: int = 5
+
+@app.get("/")
+def read_root():
+    return {"message": "SHL Recommender API is up 🚀"}
+
+@app.post("/recommend")
+async def recommend(payload: PromptRequest):
+    recommendations = recommend_tests(payload.prompt, payload.top_k)
+    results = []
+    for score, test in recommendations:
+        results.append({
+            "name": test.get("name", "Unnamed"),
+            "link": test.get("link", "#"),
+            "duration": test.get("assessment_length", "N/A"),
+            "keys": test.get("keys", []),
+            "score": score,
+            "remote": test.get("is_remote", False),
+            "adaptive": test.get("is_adaptive", False),
+            "job_levels": test.get("job_levels", []),
+            # "description": test.get("description", "No description available.")
+        })
+    return {"recommendations": results}

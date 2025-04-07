02f5168d5dfe1e7093c6911d19601ee99ba44ed9
scopophobic <adithyanmadhu1234@gmail.com>
2025-04-07 11:05:49 +0530

    okay

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Or specify like ["http://localhost:3000"]
+    allow_origins=["http://localhost:3000"],  # Or specify like ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],  # OR ["GET", "POST", "OPTIONS"]
     allow_headers=["*"],
